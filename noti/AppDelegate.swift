a94e188f0244223493a6288eb9d4952b4b458e88
AlienIdeology <wcliaw610@gmail.com>
2022-03-06 19:17:05 -0600

    added menubar icon (circle 1), icon menu (quit button), and hid application on dock

--- noti/AppDelegate.swift
+++ noti/AppDelegate.swift
@@ -7,14 +7,39 @@
 
 import Cocoa
 
-@main
+//@main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    
-
+//    private var window: NSWindow!
+    private var statusItem: NSStatusItem!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+//        window = NSWindow(
+//            contentRect: NSRect(x: 0, y: 0, width: 480, height: 270),
+//            styleMask: [.miniaturizable, .closable, .resizable, .titled],
+//            backing: .buffered, defer: false)
+//        window.center()
+//        window.title = "No Storyboard Window"
+//        window.makeKeyAndOrderFront(nil)
+        
+        // add status item
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        if let button = statusItem.button { // optional unwrapping of button
+            button.image = NSImage(systemSymbolName: "1.circle", accessibilityDescription: "1")     // SF symbol
+        }
+        
+        setupMenus()
+    }
+    
+    // function to set up menubar icon
+    func setupMenus() {
+        let menu = NSMenu()
+        
+        let quit = NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
+        menu.addItem(quit)
+        
+        statusItem.menu = menu
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
---
+++ noti/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  noti
+//
+//  Created by Allen Liao on 3/6/22.
+//
+
+import Foundation
+import AppKit
+
+let app = NSApplication.shared
+let delegate = AppDelegate()
+app.delegate = delegate
+
+_ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
+
+
